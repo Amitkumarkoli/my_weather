4950b48e9dbba6c888ec22b4ba4ffe67db580946
Amitkumarkoli <akdev523@gmail.com>
2024-01-17 14:02:03 +0530

    modify main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'Screens/show_weather.dart';
+import 'package:my_weather/Screens/onboarding_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,86 +8,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Weather App',
       debugShowCheckedModeBanner: false,
-      home: WeatherApp(),
-    );
-  }
-}
-
-class WeatherApp extends StatefulWidget {
-  const WeatherApp({super.key});
-
-  @override
-  State<WeatherApp> createState() => WeatherAppState();
-}
-
-class WeatherAppState extends State<WeatherApp> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          Image.asset(
-            'assets/images/weatherimage2.jpeg',
-            fit: BoxFit.cover, //  set image in entire screen
-            width: double.infinity,
-            height: double.infinity,
-          ),
-          const Padding(
-            padding: EdgeInsets.only(top: 180.0),
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Weather \nForecasts',
-                      style: TextStyle(
-                        fontSize: 60.0,
-                        fontWeight: FontWeight.bold,
-                      )
-                    ),
-                  SizedBox(height: 10.0),
-                  Text(
-                    'Has accurate weather information \nfor anytime and anywhere',
-                    style: TextStyle(
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.normal,
-                    )
-                  )
-                ],
-              ),    
-            ),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Padding(
-              padding: const EdgeInsets.only(bottom: 72.0),
-              child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const ShowWeather()));
-                  },
-                  style: ElevatedButton.styleFrom(
-                    fixedSize: const Size(300.0, 50.0),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20.0)),
-                    backgroundColor: const Color.fromARGB(255, 150, 116, 53),
-                    elevation: 20,
-                  ),
-                  child: const Text(
-                    'Get Started!',
-                    
-                    style: TextStyle(fontSize: 20.0,
-                    color: Colors.white),
-                    
-                  )),
-            ),
-          ),
-        ],
-      ),
+      home: OnboardingScreen(),
     );
   }
 }
