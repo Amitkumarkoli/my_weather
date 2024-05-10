1f59083d3ef4a026448693139137141b3e4a72c9
Amitkumarkoli <akdev523@gmail.com>
2024-05-10 12:18:55 +0530

    connect app to the firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,23 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:my_weather/Screens/onboarding_screen.dart';
 
 
-void main() => runApp(const MyApp());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  Platform.isAndroid
+  ? await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyBt8zt6_DkFQMecXsuUgzVGdzH1JaFA5zI", 
+        appId: "1:619873837181:android:076ce64ab30357bf785114", 
+        messagingSenderId: "619873837181", 
+        projectId: "my-weather-f45e4"
+      ))
+    : await Firebase.initializeApp();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
