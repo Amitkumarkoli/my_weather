f63b49968632d25e6e7975213ed395d5482045b0
Amitkumarkoli <akdev523@gmail.com>
2024-06-02 10:51:47 +0530

    revert some minor changes

--- lib/Screens/signup.dart
+++ lib/Screens/signup.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:my_weather/Screens/wrapper.dart';
-// import 'package:my_weather/widget/custom_cupertino_text_field.dart';
 
 class SignUp extends StatefulWidget {
   const SignUp({super.key});
@@ -12,12 +11,9 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUp> {
-  // final TextEditingController emailIDController = TextEditingController();
-  // final TextEditingController passwordController = TextEditingController();
-  // final Color bgColor = Color.fromRGBO(14, 98, 254, 1);
 
-  TextEditingController email = TextEditingController();
-  TextEditingController password = TextEditingController();
+  final TextEditingController email = TextEditingController();
+  final TextEditingController password = TextEditingController();
 
   SignUp()async{
     await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email.text, password: password.text);
@@ -26,7 +22,6 @@ class _SignUpScreenState extends State<SignUp> {
 
   @override
   Widget build(BuildContext context) {
-   // TextTheme textTheme = Theme.of(context).textTheme;
 
     return Scaffold(
       appBar: AppBar(title: Text('SignUP'),),
@@ -60,80 +55,6 @@ class _SignUpScreenState extends State<SignUp> {
           ],
         ),
       ),
-      // color: bgColor,
-      // child: SafeArea(
-      //   bottom: false,
-      //   child: Scaffold(
-      //     backgroundColor: bgColor,
-      //     body: Column(
-      //       children: [
-      //         Padding(
-      //           padding: const EdgeInsets.symmetric(vertical: 30.0),
-      //           child: Image.asset(
-      //             "assets/images/man.png",
-      //             width: 150,
-      //           ),
-      //         ),
-      //         Padding(
-      //           padding: const EdgeInsets.symmetric(horizontal: 10.0),
-      //           child: Column(
-      //             mainAxisSize: MainAxisSize.max,
-      //             mainAxisAlignment: MainAxisAlignment.center,
-      //             crossAxisAlignment: CrossAxisAlignment.stretch,
-      //             children: [
-      //               Padding(
-      //                 padding: const EdgeInsets.symmetric(vertical: 10.0),
-      //                 child: Center(
-      //                     child: Text(
-      //                   "Sign Up For Weather Updates!!",
-      //                   style:
-      //                       textTheme.labelLarge!.copyWith(color: Colors.white),
-      //                 )),
-      //               ),
-      //               //CustomCupertinoTextField(
-      //                 prefix: "Email ID",
-      //                 controller: emailIDController,
-      //               ),
-      //              // CustomCupertinoTextField(
-      //                 prefix: "Password",
-      //                 obscureText: true,
-      //                 controller: passwordController,
-      //               ),
-      //               Padding(
-      //                 padding: const EdgeInsets.symmetric(vertical: 10.0),
-      //                 child: FilledButton(
-      //                   onPressed: () {
-      //                     Navigator.push(
-      //                         context,
-      //                         MaterialPageRoute(
-      //                             builder: (context) => HomePage()));
-      //                   },
-      //                   child: Text("Sign Up"),
-      //                   style: ButtonStyle(
-      //                       backgroundColor: MaterialStatePropertyAll(
-      //                           Color.fromRGBO(225, 225, 115, 1)),
-      //                       foregroundColor: MaterialStatePropertyAll(bgColor)),
-      //                 ),
-      //               ),
-      //               Padding(
-      //                 padding: const EdgeInsets.symmetric(vertical: 2.0),
-      //                 child: TextButton(
-      //                   onPressed: () {
-      //                     Navigator.pop(context);
-      //                   },
-      //                   child: Text("Back"),
-      //                   style: ButtonStyle(
-      //                       foregroundColor:
-      //                           MaterialStatePropertyAll(Colors.white)),
-      //                 ),
-      //               )
-      //             ],
-      //           ),
-      //         ),
-      //       ],
-      //     ),
-      //   ),
-    //   ),
     );
   }
 }
